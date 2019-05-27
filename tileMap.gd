04f016b312dca7abbb8d22b0dd8274a3ef6bd454
CloudRex <cloudrex@outlook.com>
2019-05-27 12:51:39 -0400

    Improve terrain generation

--- tileMap.gd
+++ tileMap.gd
@@ -1,9 +1,49 @@
 extends TileMap
 
-const _tileIdGrass = 2
+enum Tile {STONE = 0, COPPER = 1, GRASS = 2}
+
+var rarity = {Tile.GRASS: 15, Tile.STONE: 5, Tile.COPPER: 80}
+var tiles = [Tile.GRASS, Tile.STONE, Tile.COPPER]
 
 func _ready():
+	generate()
 	print("TileMap ready")
+	
+	
+func getNextTile():
+	# Generate a random number from 1-100.
+	var pick = randi() % 100 + 1
+	
+	# Create the resulting tile buffer.
+	var result = tiles[0]
+	
+	for tile in tiles:
+		if (pick >= rarity[tile]):
+			result = tile
+			
+			break
+			
+	return result
+	
+func generate():
+	var start = Vector2(-16, -16)
+	var end = Vector2(16, 16)
+	var buffer = start
+	
+	# Column.
+	while (buffer.x != end.x):
+		self.set_cellv(buffer, getNextTile())
+		buffer.x += 1
+		
+		# Row.
+		while (buffer.y != end.y):
+			self.set_cellv(buffer, getNextTile())
+			buffer.y += 1
+		
+		# Reset row counter.
+		buffer.y = start.y
+		
+	print("Terrain generation completed")
 
 func _process(delta):
 	if (Input.is_mouse_button_pressed(BUTTON_LEFT)):
@@ -11,7 +51,6 @@ func _process(delta):
 		var location = self.world_to_map(mousePosition)
 		var cell = self.get_cell(location.x, location.y)
 		
-		if (cell != -1):
-			print(self.tile_set.tile_get_name(cell))
-		else:
-			self.set_cell(location.x, location.y, _tileIdGrass)
+		if (cell == -1):
+			print("Set cell at: (" + str(location.x) + ", " + str(location.y) + ")")
+			self.set_cell(location.x, location.y, Tile.GRASS)
