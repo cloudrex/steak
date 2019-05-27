a460f82ea6d02eb218f7cd46b581eace82ed6ec8
CloudRex <cloudrex@outlook.com>
2019-05-27 12:24:49 -0400

    On click add tile instead of hover

--- tileMap.gd
+++ tileMap.gd
@@ -13,11 +13,12 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var mousePosition = get_global_mouse_position()
-	var location = world_to_map(mousePosition)
-	var cell = get_cell(location.x, location.y)
-	
-	if (cell != -1):
-		print(self.tile_set.tile_get_name(cell))
-	else:
-		set_cell(location.x, location.y, _tileIdGrass)
+	if (Input.is_mouse_button_pressed(BUTTON_LEFT)):
+		var mousePosition = get_global_mouse_position()
+		var location = self.world_to_map(mousePosition)
+		var cell = self.get_cell(location.x, location.y)
+		
+		if (cell != -1):
+			print(self.tile_set.tile_get_name(cell))
+		else:
+			self.set_cell(location.x, location.y, _tileIdGrass)
