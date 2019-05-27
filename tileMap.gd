cca867fe2fd29a9c620b36dbf23c7a0b3023ee07
CloudRex <cloudrex@outlook.com>
2019-05-27 15:51:24 -0400

    Do not initially generate terrain

--- tileMap.gd
+++ tileMap.gd
@@ -9,9 +9,6 @@ var tileShortcutCount = tileShortcuts.size()
 var activeTile = tiles[0]
 
 func _ready():
-	# Invoke terrain generation function.
-	generate()
-	
 	# Inform the developer.
 	print("TileMap ready")
 	
